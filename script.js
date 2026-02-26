adeaef48b54b04505fb9e0b3532958d07f517580
amosuniradatullahi9-cpu <amosuniradatullahi9@gmail.com>
2026-02-26 10:45:25 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
-const button = document.querySelector(".toggle-btn");
-const box = document.getElementById("account-details");
+document.addEventListener("DOMContentLoaded", function() {
+    const button = document.querySelector(".toggle-btn");
+    const box = document.getElementById("account-details");
 
-button.addEventListener("click", function () {
-    box.classList.toggle("show");
+    button.addEventListener("click", function () {
+        box.classList.toggle("show");
 
-    if (box.classList.contains("show")) {
-        button.textContent = "Hide Account Details";
-    } else {
-        button.textContent = "Show Account Details";
-    }
+        if (box.classList.contains("show")) {
+            button.textContent = "Hide Account Details";
+        } else {
+            button.textContent = "Show Account Details";
+        }
+    });
 });
